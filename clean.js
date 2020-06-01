3a9cf16a689665aaa278f4b0a22f8b1e7436e846
toghrulgasimov <toghrulgasimov@gmail.com>
2020-06-01 18:44:43 +0200

    dsddd

--- clean.js
+++ clean.js
@@ -9,45 +9,12 @@ let mongoUtil = require( './mongoUtil' );
     let db = await mongoUtil.getDb();
 
     let index = 0;
-    let k = db.collection('adlar2').find({});
-    let S = new Set();
-    let cnt = await k.count();
-    console.log(cnt);
-    k.forEach(async function (d) {
-        index++;
-        delete d._id;
-        S.add(JSON.stringify(d));
-        if(index % 100000 == 0) {
-            console.log(S.size);
-        }
-        if(index == cnt-1) {
-            console.log('--------Bang' + S.size);
-            let index2 = 0;
-            let ans = [];
-            for(let x of S.values()) {
-                index2++;
-                x = x.split("İ").join('i');
-                x = x.split("İ").join('i');
-                x = x.split("Ə").join('e');
-                x = x.split("Ö").join('o');
-                x = x.split("Ğ").join('g');
-                x = x.split("Ç").join('c');
-                x = x.split("Ş").join('s');
-                x = x.split("Ü").join('u');
-                x = x.split("I").join('i');
-                x = x.toLowerCase();
-                let o = JSON.parse(x);
-                ans.push(o);
-                if(index2 % 10000 == 0) {
-                    console.log(o);
-                    await db.collection("adlar3").insertMany(ans);
-                    ans = [];
-                }
-                //console.log(x);
-            }
-            await db.collection("adlar3").insertMany(ans);
-            ans = [];
-            console.log("DONE");
-        }
-    })
+    let k = db.collection('adlar3').find({});
+
+    for(let i = 1800; i <= 2020; i++) {
+        let r = await db.collection("adlar2").updateMany({te:i+""}, {$set:{te:i}});
+        console.log(r);
+        console.log(i);
+    }
+    console.log("DONEEEEE");
 })();
