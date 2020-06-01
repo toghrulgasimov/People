fcf061d60b1b4f1f4ae320e8f5be81d5a4c241e6
toghrulgasimov <toghrulgasimov@gmail.com>
2020-06-01 20:05:03 +0200

    jhjh

--- server.js
+++ server.js
@@ -19,15 +19,26 @@ async function f() {
         res.send(s);
     })
     app.get('/qu', async (req, res) => {
+
         console.log(req.query);
+        if(req.query.te != undefined) {
+            req.query.te = parseInt(req.query.te);
+        }
         if(req.query.rayon != undefined) {
             req.query.rayon = {$regex: ".*"+req.query.rayon+".*"}
-            console.log(req.query);
         }
         if(req.query.adres != undefined) {
-            req.query.adres = {$regex: ".*"+req.query.adres+".*"}
-            console.log(req.query);
+            req.query.adres = {$regex: ".*"+req.query.adres+".*"};
         }
+        if(req.query.dogumMin != undefined) {
+            req.query.te = {$gte: parseInt(req.query.dogumMin)};
+
+        }
+        if(req.query.dogumMax != undefined) {
+            req.query.te = {$lte: parseInt(req.query.dogumMax)};
+        }
+        delete req.query.dogumMin;
+        delete req.query.dogumMax;
         let dd = await db.collection('adlar3').find(req.query).limit(1000).toArray();
         let data = {d : dd};
         console.log(dd);
